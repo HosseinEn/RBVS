51a9b857e927ce4b2c68d5494d5697813ba1ee67
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-06-04 21:50:32 +0330

    remove useless assignments and stick to defaults

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ def main(config):
     if not os.path.exists(config.result_path):
         os.makedirs(config.result_path)
     
-    lr = random.random()*0.0005 + 0.0000005
-    augmentation_prob= random.random()*0.7
-    epoch = random.choice([100,150,200,250])
-    decay_ratio = random.random()*0.8
-    decay_epoch = int(epoch*decay_ratio)
+    # lr = random.random()*0.0005 + 0.0000005
+    # augmentation_prob= random.random()*0.7
+    # epoch = random.choice([100,150,200,250])
+    # decay_ratio = random.random()*0.8
+    # decay_epoch = int(epoch*decay_ratio)
 
     config.augmentation_prob = augmentation_prob
     config.num_epochs = epoch
