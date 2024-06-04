412da73d92cf9bf7a768d72bb0157dc50836ef5b
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-06-04 17:16:34 +0330

    add kaggle directories

--- colab.sh
+++ colab.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/LeeJunHyun/Image_Segmentation.git
 wget https://staffnet.kingston.ac.uk/~ku15565/CHASE_DB1/assets/CHASEDB1.zip
-unzip -d /content/CHASEDB1 /content/CHASEDB1.zip
+unzip -d /kaggle/working/CHASEDB1 /kaggle/working/CHASEDB1.zip
