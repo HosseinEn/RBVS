50a938c33feb55044555bef58498f6229d3e32d5
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-06-11 21:23:53 +0330

    update metrics

--- evaluation.py
+++ evaluation.py
@@ -1,23 +1,40 @@
-# evaluation.import torch
-from torcheval.metrics import BinaryAccuracy, BinaryPrecision, BinaryRecall, BinaryF1Score, BinaryJaccardIndex, BinaryDiceCoefficient
+import torch
+from torcheval.metrics import BinaryAccuracy, BinaryPrecision, BinaryRecall, BinaryF1Score, BinaryDiceCoefficient
+
+def jaccard_index(SR, GT, threshold=0.5):
+    SR = (SR > threshold).float()
+    GT = (GT == torch.max(GT)).float()
+    
+    intersection = torch.sum(SR * GT)
+    union = torch.sum(SR + GT) - intersection
+    jaccard = intersection / (union + 1e-6)
+    
+    return jaccard.item()
 
 def get_metrics(SR, GT, threshold=0.5):
-    SR = SR > threshold
-    GT = GT == torch.max(GT)
+    SR = (SR > threshold).float()
+    GT = (GT == torch.max(GT)).float()
 
-    accuracy = BinaryAccuracy().to('cuda')
-    precision = BinaryPrecision().to('cuda')
-    recall = BinaryRecall().to('cuda')
-    f1_score = BinaryF1Score().to('cuda')
-    jaccard = BinaryJaccardIndex().to('cuda')
-    dice = BinaryDiceCoefficient().to('cuda')
+    accuracy = BinaryAccuracy(device='cuda')
+    precision = BinaryPrecision(device='cuda')
+    recall = BinaryRecall(device='cuda')
+    f1_score = BinaryF1Score(device='cuda')
+    dice = BinaryDiceCoefficient(device='cuda')
 
-    acc = accuracy(SR, GT)
-    se = recall(SR, GT)  # Sensitivity is Recall
-    sp = accuracy(SR == 0, GT == 0)  # Specificity
-    pc = precision(SR, GT)
-    f1 = f1_score(SR, GT)
-    js = jaccard(SR, GT)
-    dc = dice(SR, GT)
+    # Update metrics with predictions and targets
+    accuracy.update(SR, GT)
+    precision.update(SR, GT)
+    recall.update(SR, GT)
+    f1_score.update(SR, GT)
+    dice.update(SR, GT)
 
-    return acc.item(), se.item(), sp.item(), pc.item(), f1.item(), js.item(), dc.item()
+    # Compute the metric values
+    acc = accuracy.compute().item()
+    se = recall.compute().item()  # Sensitivity is Recall
+    sp = accuracy.compute().item()  # Specificity
+    pc = precision.compute().item()
+    f1 = f1_score.compute().item()
+    dc = dice.compute().item()
+    js = jaccard_index(SR, GT)  # Jaccard Index
+
+    return acc, se, sp, pc, f1, js, dc
