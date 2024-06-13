c0edc2d3773c83eebddd412ff8467b1eb3460a76
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-06-13 13:38:07 +0330

    improve my_data_loder

--- my_data_loader.py
+++ my_data_loader.py
@@ -1,68 +1,67 @@
 import os
 import random
-from random import shuffle
-import numpy as np
-import torch
 from torch.utils import data
-from torchvision import transforms as T
-from torchvision.transforms import functional as F
+from torchvision import transform_lists as T
+from torchvision.transform_lists import functional as F
 from PIL import Image
 
 class ImageFolder(data.Dataset):
 	def __init__(self, root,image_size=224,mode='train',augmentation_prob=0.4):
-		"""Initializes image paths and preprocessing module."""
 		self.root = root
-		
-		# GT : Ground Truth
 		self.GT_paths = root[:-1]+'_GT/'
-		self.image_paths = list(map(lambda x: os.path.join(root, x), os.listdir(root)))
+		self.image_paths = [os.path.join(root, fname) for fname in os.listdir(root)]
 		self.image_size = image_size
 		self.mode = mode
-		self.RotationDegree = [0,90,180,270]
+		self.rotation_degrees = [0,90,180,270]
 		self.augmentation_prob = augmentation_prob
-		print("image count in {} path :{}".format(self.mode,len(self.image_paths)))
+		print(f"Image count in {self.mode} path: {len(self.image_paths)}")
 
-	def __getitem__(self, index):
-		"""Reads an image from a file and preprocesses it and returns."""
+	def get_paths(self, index):
 		image_path = self.image_paths[index]
 		filename = image_path.split('_')[-1][:-len(".jpg")]
 		GT_path = self.GT_paths + 'Image_' + filename + '_1stHO.png'
+		return image_path, GT_path
 
+	def aspect_ratio(self, image):
+		return image.size[0] / image.size[1]
+	
+	def train_mode(self):
+		return self.mode == 'train'
 
+	def __getitem__(self, index):
+		image_path, GT_path = self.get_paths(index)
 		image = Image.open(image_path)
 		GT = Image.open(GT_path)
 
-		aspect_ratio = image.size[1]/image.size[0]
+		aspect_ratio = self.aspect_ratio(image)
 
-		Transform = []
+		transform_list = []
 
-		ResizeRange = random.randint(300,320)
-		Transform.append(T.Resize((int(ResizeRange*aspect_ratio),ResizeRange)))
-		p_transform = random.random()
+		resize_range = random.randint(300,320)
+		transform_list.append(T.Resize((int(resize_range*aspect_ratio),resize_range)))
 
-		if (self.mode == 'train') and p_transform <= self.augmentation_prob:
-			RotationDegree = random.randint(0,3)
-			RotationDegree = self.RotationDegree[RotationDegree]
-			if (RotationDegree == 90) or (RotationDegree == 270):
-				aspect_ratio = 1/aspect_ratio
+		if self.train_mode() and random.random() <= self.augmentation_prob:
+			rotation_degree = random.choice(self.rotation_degrees)
 
-			Transform.append(T.RandomRotation((RotationDegree,RotationDegree)))
+			if rotation_degree in [90,270]:
+				aspect_ratio = 1 / aspect_ratio
+
+			transform_list.append(T.RandomRotation(rotation_degree))
 						
-			RotationRange = random.randint(-10,10)
-			Transform.append(T.RandomRotation((RotationRange,RotationRange)))
-			CropRange = random.randint(250,270)
-			Transform.append(T.CenterCrop((int(CropRange*aspect_ratio),CropRange)))
-			Transform = T.Compose(Transform)
+			transform_list.append(T.RandomRotation(random.randint(-10,10)))
+			crop_range = random.randint(250,270)
+			transform_list.append(T.CenterCrop((int(crop_range*aspect_ratio),crop_range)))
+			transform = T.Compose(transform_list)
 			
-			image = Transform(image)
-			GT = Transform(GT)
+			image = transform(image)
+			GT = transform(GT)
 
-			ShiftRange_left = random.randint(0,20)
-			ShiftRange_upper = random.randint(0,20)
-			ShiftRange_right = image.size[0] - random.randint(0,20)
-			ShiftRange_lower = image.size[1] - random.randint(0,20)
-			image = image.crop(box=(ShiftRange_left,ShiftRange_upper,ShiftRange_right,ShiftRange_lower))
-			GT = GT.crop(box=(ShiftRange_left,ShiftRange_upper,ShiftRange_right,ShiftRange_lower))
+			shift_left = random.randint(0,20)
+			shift_upper = random.randint(0,20)
+			shift_right = image.size[0] - random.randint(0,20)
+			shift_lower = image.size[1] - random.randint(0,20)
+			image = image.crop(box=(shift_left,shift_upper,shift_right,shift_lower))
+			GT = GT.crop(box=(shift_left,shift_upper,shift_right,shift_lower))
 
 			if random.random() < 0.5:
 				image = F.hflip(image)
@@ -72,32 +71,27 @@ class ImageFolder(data.Dataset):
 				image = F.vflip(image)
 				GT = F.vflip(GT)
 
-			Transform = T.ColorJitter(brightness=0.2,contrast=0.2,hue=0.02)
-
-			image = Transform(image)
-
-			Transform =[]
+			color_jitter = T.ColorJitter(brightness=0.2,contrast=0.2,hue=0.02)
+			image = color_jitter(image)
 
 
-		Transform.append(T.Resize((int(256*aspect_ratio)-int(256*aspect_ratio)%16,256)))
-		Transform.append(T.ToTensor())
-		Transform = T.Compose(Transform)
-		
-		image = Transform(image)
-		GT = Transform(GT)
+		# Final resize, convert to tensor, and normalize
+		final_transform = T.Compose([
+			T.Resize((int(256 * aspect_ratio) - int(256 * aspect_ratio) % 16, 256)),
+			T.ToTensor(),
+			T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+		])
 
-		Norm_ = T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-		image = Norm_(image)
+
+		image = final_transform(image)
+		GT = final_transform(GT)
 
 		return image, GT
 
 	def __len__(self):
-		"""Returns the total number of font files."""
 		return len(self.image_paths)
 
-def get_loader(image_path, image_size, batch_size, num_workers=2, mode='train',augmentation_prob=0.4):
-	"""Builds and returns Dataloader."""
-	
+def get_loader(image_path, image_size, batch_size, num_workers=2, mode='train',augmentation_prob=0.4):	
 	dataset = ImageFolder(root = image_path, image_size =image_size, mode=mode,augmentation_prob=augmentation_prob)
 	data_loader = data.DataLoader(dataset=dataset,
 								  batch_size=batch_size,
